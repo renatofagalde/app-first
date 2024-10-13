943d6bb3c8941248b219e1197346bed01b63abd9
renato@likwi.com.br <renatofagalde>
2024-10-13 19:09:47 -0300

    role for lambda

--- create-app.sh
+++ create-app.sh
@@ -31,33 +31,42 @@ sed -i "s/PROD_STACK_NAME: .*/PROD_STACK_NAME: ${stack_name}/g" "$repo_name"/.gi
 echo "#L7: Substituindo o nome da stack no arquivo samconfig.toml..."
 sed -i "s/stack_name = .*/stack_name = \"${stack_name}\"/g" "$repo_name"/samconfig.toml
 
-# L8: Renomear o diretório do módulo para 'bootstrap'
-echo "#L8: Renomeando o diretório do módulo para 'bootstrap'..."
-mv "$repo_name"/hello-world "$repo_name"/bootstrap
+# L8: Renomear o diretório do código para o nome do repositório
+echo "#L8: Renomeando o diretório do código para '$repo_name'..."
+mv "$repo_name"/hello-world "$repo_name/$repo_name"
 
-# L9: Substituir o nome do módulo no go.mod para 'bootstrap'
-echo "#L9: Substituindo o nome do módulo no go.mod para 'bootstrap'..."
-sed -i "s/module .*/module bootstrap/g" "$repo_name"/bootstrap/go.mod
+# L9: Substituir o nome do módulo no go.mod para o nome do repositório
+echo "#L9: Substituindo o nome do módulo no go.mod para '$repo_name'..."
+sed -i "s/module .*/module $repo_name/g" "$repo_name/$repo_name/go.mod"
 
-# L10: Atualizar o template.yaml para incluir o nome da stack no recurso da Lambda
-echo "#L10: Atualizando o template.yaml para incluir o nome da stack no recurso da Lambda..."
-sed -i "s/FunctionName: .*/FunctionName: ${stack_name}-lambda/g" "$repo_name"/template.yaml
+# L10: Atualizar o template.yaml para substituir HelloWorld pelo nome do repositório em letras minúsculas
+echo "#L10: Atualizando o template.yaml para substituir HelloWorld pelo nome do repositório..."
+lowercase_repo_name=$(echo "$repo_name" | tr '[:upper:]' '[:lower:]')  # Converte o nome do repositório para letras minúsculas
+sed -i "s/HelloWorld/${lowercase_repo_name}/g" "$repo_name/template.yaml"  # Substitui HelloWorld pelo nome do repositório
 
-# L11: Adicionar o novo repositório remoto com o nome "github"
-echo "#L11: Adicionando o novo repositório remoto com o nome 'github'..."
+# L11: Atualizar o template.yaml para substituir o nome da LambdaExecutionRole pelo nome da stack
+echo "#L11: Atualizando o template.yaml para substituir o nome da LambdaExecutionRole..."
+sed -i "s/LambdaExecutionRole/${stack_name}-LambdaExecutionRole/g" "$repo_name/template.yaml"  # Substitui o nome da role
+
+# L12: Atualizar o template.yaml para substituir o nome da função HelloWorldFunction pelo nome do repositório
+echo "#L12: Atualizando o template.yaml para substituir o nome da função..."
+sed -i "s/HelloWorldFunction/${lowercase_repo_name}function/g" "$repo_name/template.yaml"  # Substitui HelloWorldFunction pelo nome do repositório
+
+# L13: Adicionar o novo repositório remoto com o nome "github"
+echo "#L13: Adicionando o novo repositório remoto com o nome 'github'..."
 cd "$repo_name" || exit
 git remote add github https://github.com/renatofagalde/"$repo_name".git
 
-# L12: Push inicial para o novo repositório
-echo "#L12: Realizando o push inicial para o novo repositório..."
+# L14: Push inicial para o novo repositório
+echo "#L14: Realizando o push inicial para o novo repositório..."
 git push github main
 
-# L13: Criar os segredos no novo repositório
-echo "#L13: Criando os segredos no novo repositório..."
+# L15: Criar os segredos no novo repositório
+echo "#L15: Criando os segredos no novo repositório..."
 REPO="renatofagalde/$repo_name"  # Define o repositório no formato correto
 echo "$GITHUB_AWS_ACCESS_KEY_ID" | gh secret set AWS_ACCESS_KEY_ID --repo "$REPO"  # Definindo o segredo AWS_ACCESS_KEY_ID
 echo "$GITHUB_AWS_SECRET_ACCESS_KEY" | gh secret set AWS_SECRET_ACCESS_KEY --repo "$REPO"  # Definindo o segredo AWS_SECRET_ACCESS_KEY
 
-# L14: Informar o sucesso
-echo "#L14: Informando o sucesso..."
+# L16: Informar o sucesso
+echo "#L16: Informando o sucesso..."
 echo "Repository '$repo_name' and stack '$stack_name' created successfully with AWS secrets configured!"
